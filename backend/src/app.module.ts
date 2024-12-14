239ad2e1777d0901b283b0d5a737bf3bbe8ca352
carlsoncs <c.s.carlson6@gmail.com>
2024-12-14 18:14:24 -0500

    Runs without errors, only working module is employees

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import config module from nestjs/config 
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { EmployeesModule } from './employees/employees.module';
 import { TimeclockModule } from './timeclock/timeclock.module';
@@ -12,30 +12,23 @@ import { ProductionModule } from './production/production.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
+    ConfigModule.forRoot(), // Load .env variables
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Set to false in production!
+      }),
     }),
-
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true, // Set to false in production!
-    }),
-
-    AuthModule,
-
     EmployeesModule,
-
-    TimeclockModule,
-
-    ProductionModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/employees/employees.module.ts
+++ backend/src/employees/employees.module.ts
@@ -3,8 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './employee.entity';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
-import { EmployeesController } from './employees.controller';
-import { EmployeesService } from './employees.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Employee])],
